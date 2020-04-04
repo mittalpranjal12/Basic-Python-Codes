1a0ef54b50cdc90e8e9c57c56cc142f37a90fcd2
pranjal mittal <pranjalmittal61117@gmail.com>
2020-04-04 23:20:29 +0530

    Add files via upload

---
+++ add_two_numbers.py
@@ -0,0 +1,8 @@
+#python code to add two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+
+print("Sum of {0} and {1} is: {2} " .format(a ,b ,sum))
